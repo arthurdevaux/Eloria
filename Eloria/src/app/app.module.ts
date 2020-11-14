81e511c4b7007f767a6ec93d4bef10fd765716c9
arthurdevaux <arthur.devaux31@gmail.com>
2020-11-14 17:00:01 +0100

    basic archi

--- Eloria/src/app/app.module.ts
+++ Eloria/src/app/app.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { ToolbarComponent } from './componnents/toolbar/toolbar.component';
+import { InputComponent } from './componnents/input/input.component';
+import { HomeComponent } from './Pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ToolbarComponent,
+    InputComponent
   ],
   imports: [
     BrowserModule,
